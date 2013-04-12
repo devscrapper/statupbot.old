8851e151138ffd1fb199b116aea3a5cabe92a44e
dave scrapper <devscrapper@yahoo.fr>
2013-04-12 18:25:34 +0200

    v0.1 input flow server installé on recupère les fichiers publiés par enginebot

---
+++ model/building/inputs.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby -w
+# encoding: UTF-8
+require 'socket'
+require "ruby-progressbar"
+
+module Flowing
+
+  SEPARATOR1=";"
+  SEPARATOR2="|"
+  SEPARATOR3=","
+  EOFLINE ="\n"
+
+  class Inputs
+
+    def initialize()
+
+    end
+
+    def toto(inputflow)
+      p inputflow
+    end
+  end
+
+end
---
+++ run/input_flows_server.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby -w
+# encoding: UTF-8
+require 'yaml'
+require_relative '../lib/logging'
+require_relative '../model/flowing/flow_connection'
+
+
+#--------------------------------------------------------------------------------------------------------------------
+# INIT
+#--------------------------------------------------------------------------------------------------------------------
+PARAMETERS = File.dirname(__FILE__) + "/../parameter/" + File.basename(__FILE__, ".rb") + ".yml"
+ENVIRONMENT= File.dirname(__FILE__) + "/../parameter/environment.yml"
+listening_port = 9105 # port d'ecoute
+task_server_port = 9101
+$staging = "production"
+$debugging = false
+#--------------------------------------------------------------------------------------------------------------------
+# INPUT
+#--------------------------------------------------------------------------------------------------------------------
+begin
+  environment = YAML::load(File.open(ENVIRONMENT), "r:UTF-8")
+  $staging = environment["staging"] unless environment["staging"].nil?
+rescue Exception => e
+  STDERR << "loading parameter file #{ENVIRONMENT} failed : #{e.message}"
+end
+
+begin
+  params = YAML::load(File.open(PARAMETERS), "r:UTF-8")
+  listening_port = params[$staging]["listening_port"] unless params[$staging]["listening_port"].nil?
+  task_server_port = params[$staging]["task_server_port"] unless params[$staging]["task_server_port"].nil?
+  $debugging = params[$staging]["debugging"] unless params[$staging]["debugging"].nil?
+rescue Exception => e
+  STDERR << "loading parameters file #{PARAMETERS} failed : #{e.message}"
+end
+
+logger = Logging::Log.new(self, :staging => $staging, :id_file => File.basename(__FILE__, ".rb"), :debugging => $debugging)
+
+Logging::show_configuration
+logger.a_log.info "parameters of input flows server :"
+logger.a_log.info "listening port : #{listening_port}"
+logger.a_log.info "task server port : #{task_server_port}"
+logger.a_log.info "debugging : #{$debugging}"
+logger.a_log.info "staging : #{$staging}"
+
+include Flowing
+#--------------------------------------------------------------------------------------------------------------------
+# MAIN
+#--------------------------------------------------------------------------------------------------------------------
+EventMachine.run {
+  Signal.trap("INT") { EventMachine.stop }
+  Signal.trap("TERM") { EventMachine.stop }
+
+  logger.a_log.info "input flows server is starting"
+  EventMachine.start_server "localhost", listening_port, FlowConnection, logger
+}
+logger.a_log.info "input flows server stopped"
+
