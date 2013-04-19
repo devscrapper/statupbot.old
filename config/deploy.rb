dd4f72f9f0944e22969b6ad4f80a2cb64455916d
dave scrapper <devscrapper@yahoo.fr>
2013-04-19 18:37:40 +0200

    v0.4 report de la meo de archive_previous  correction de bug archive_previous : suppression de dernier / pour les constante INPUT TMP OUTPUT et ajout du File.join pour calculer le chemin absolu dans Flow.last et Flow.list

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,8 +3,8 @@ set :branch, "master" # version à déployer
 set :application, "statupbot" # nom application (github)
 set :keep_releases, 3 # nombre de version conservées
 set :server_name, "192.168.1.53" # adresse du server de destination
-#set :repository, "https://github.com/devscrapper/#{application}.git" # adresse du referentiel de la l'application sous github
-set :repository, "d:///referentiel/dev/#{application}/.git"
+set :repository, "https://github.com/devscrapper/#{application}.git" # adresse du referentiel de la l'application sous github
+#set :repository, "d:///referentiel/dev/#{application}/.git"
 set :deploy_to, "/home/eric/www/#{application}" # repertoire de deploiement de l'application
 set :scm, "git"
 set :deploy_via, :copy
